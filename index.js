29ff09035ff4b467460d3c1676775d547fdcf246
Eric <eric.ou@yale.edu>
2020-07-15 15:53:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+const map = (arr, fn) => {
+    let copy = [...arr];
+    for (let index in arr){
+        copy[index] = fn(copy[index]);
+    }
+    return copy;
+}
+
+const reduce = (arr, fn, start = arr[0]) =>{
+    var value = (start == arr[0]) ? start : fn(start, arr[0]);
+    for (let val of arr.slice(1)){
+        value = fn(value, val)
+    }
+    return value;
+}
