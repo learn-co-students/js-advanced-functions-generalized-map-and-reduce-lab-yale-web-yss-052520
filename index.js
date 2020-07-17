33eb5923119106dd023ee82009b6debe09263640
adilgondal1 <adil.gondal@yale.edu>
2020-07-17 02:55:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(source,func){
+    const result = []
+    source.forEach(element => result.push(func(element)))
+    return result
+}
+
+function reduce(source, func, memo){
+    let recurser = 0; 
+    if (!memo) {
+        memo = source[0]
+        recurser = 1
+    }
+    source = source.slice(recurser)
+    source.forEach(item => memo = func(item, memo))
+    return memo 
+}
